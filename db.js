8d3e187d93abd148b20bc196d8218dff6bb4a4ea
Angie Rodriguez <angrodri902@gmail.com>
2021-02-10 22:37:52 -0800

    Modify db.js

--- db.js
+++ db.js
@@ -1,15 +1,24 @@
 const Pool = require("pg").Pool;
 require("dotenv").config();
 
+// const devConfig = {
+//   user: process.env.PG_USER,
+//   host: process.env.PG_HOST,
+//   port: process.env.PG_PORT,
+//   database: process.env.PG_DATABASE,
+// };
+
 const devConfig = {
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  port: process.env.PG_PORT,
-  database: process.env.PG_DATABASE,
+  connectionString: `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`,
 };
 
+// const proConfig = process.env.DATABASE_URL; // heroku addon
+
 const proConfig = {
-  connectionString: process.env.DATABASE_URL, // heroku addon
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 };
 
 const pool = new Pool(
